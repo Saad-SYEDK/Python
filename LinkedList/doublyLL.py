d26167818875cbbdb37e3098b6d478ad9c05a099
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2025-02-21 12:32:06 +0530

    Added comments in doublyLL.py

--- LinkedList/doublyLL.py
+++ LinkedList/doublyLL.py
@@ -1,3 +1,10 @@
+'''
+    The only difference in doubly linked list is that it contains an extra pointer which points to
+    the previous node, this requires us to make some changed in all of our methods of singly linked list
+    to handle the prev pointer.
+    No need to code for write all the methods from scratch, just implement basic mehtods like insertion and deletion
+    and you will be good to go.
+'''
 class Node:
     def __init__(self, prev, data, next):
         self.prev = prev
@@ -28,6 +35,23 @@ class DLL:
         if self.head is None:
             self.head = node
     
+    def delete_end(self):
+        # We have maintained a tail pointer, which makes our work very simple, we just upatae the node at tail pointer
+        
+        # If the list is empty, return
+        if self.head is None:
+            return
+        
+        # If the list has only one node, update both pointers
+        if self.head == self.tail:
+            self.head = self.tail = None
+            return
+        
+        # In all the other cases we just move the tail pointer to the previous node and update its next pointer to None.
+        self.tail = self.tail.prev
+        self.tail.next = None
+        return
+    
     def display(self):
         if self.head is None:
             print("Empty List!")
@@ -41,7 +65,12 @@ class DLL:
         
 # Checking
 list = DLL()
+
 list.append(1)
 list.prepend(0)
 list.append(2)
+
+list.delete_end()
+list.delete_end()
+list.delete_end()
 list.display()
