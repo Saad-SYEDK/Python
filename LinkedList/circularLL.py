d001f9cf10766257973144fe6d66835638b4e597
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2025-02-22 18:16:47 +0530

    Created a new file circularLL.py and created two classes Node and CLL with their constructers

---
+++ LinkedList/circularLL.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, data, next):
+        self.data = data
+        self.next = next
+        
+class CLL:
+    def __init__(self):
+        self.head = None
+        self.tail = None
