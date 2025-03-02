eb33d5a05e9feeee45ee2dd51b73e4750cdc2da7
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2025-03-02 13:46:39 +0530

    Created binaryTree.py and added initial comments

---
+++ Tree/binaryTree.py
@@ -0,0 +1,6 @@
+# Binary Tree(BT) is a tree that contains at most 2 children at every node
+
+# Binary Search Tree(BST) is a BT that contains any order ex: at every node 
+#       left child will be smaller than parent and right child will be greater than the parent or vice-versa
+
+ 
