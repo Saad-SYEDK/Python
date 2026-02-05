feb74405699840061429abbfa12138ef9e967192
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2026-02-05 19:29:12 +0530

    Created variables.py

--- 0_INDEX.py
+++ 0_INDEX.py
@@ -1,1 +1,3 @@
 # Here we will list all the topics we learn order wise and link the respective files to it.
+
+1. Variables
---
+++ variables.py
@@ -0,0 +1,23 @@
+# Variables are like a container to store data values.
+# In Python, we do not need to declare the data type of variable.
+
+# num is a variable
+num = 200
+# To print the value of variable, we use print() function.
+print(num) # Output: 200 
+
+# We can also change the value of variable.
+num = 300 
+print(num) # Output: 300
+
+# We can store different data types in variables.
+name = "Saad" # String data type    
+age = 20       # Integer data type
+height = 5.9   # Float data type
+is_student = True # Boolean data type
+
+# We can check the data type of variable using type() function.
+print(type(name))       # Output: <class 'str'>
+print(type(age))        # Output: <class 'int'>
+print(type(height))    # Output: <class 'float'>
+print(type(is_student)) # Output: <class 'bool'>
