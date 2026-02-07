2acff2cd79d931aa00e1b5f626bc9de7386fe6d9
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2026-02-07 11:30:18 +0530

    Added exercise in if-else

--- if-else.py
+++ if-else.py
@@ -26,3 +26,53 @@ else:
         print("Grade: B")
     else:
         print("Grade: C")
+
+"""Exercise: Condition
+Using following list of cities per country,
+india = ["mumbai", "banglore", "chennai", "delhi"]
+pakistan = ["lahore","karachi","islamabad"]
+bangladesh = ["dhaka", "khulna", "rangpur"]
+1.Write a program that asks user to enter a city name and it should tell which country the city belongs to
+2. Write a program that asks user to enter two cities and it tells you if they both are in same country or not. For example if I enter mumbai and chennai, it will print "Both cities are in India" but if I enter mumbai and dhaka it should print "They don't belong to same country"
+3. Write a python program that can tell you if your sugar is normal or not. Normal fasting level sugar range is 80 to 100.
+    Ask user to enter his fasting sugar level
+    If it is below 80 to 100 range then print that sugar is low
+    If it is above 100 then print that it is high otherwise print that it is normal"""
+    
+# Solution:
+india = ["mumbai", "banglore", "chennai", "delhi"]
+pakistan = ["lahore","karachi","islamabad"]
+bangladesh = ["dhaka", "khulna", "rangpur"]
+
+# 1. 
+city = input("Enter City name:")
+if city in india:
+    print("india")
+elif city in pakistan:
+    print("Pakistan")
+elif city in bangladesh:
+    print("bangladesh")
+else:
+    print("CIty not in list")
+    
+# 2. 
+c1 = input("Enter city 1")
+c2 = input("Enter city 2")
+
+if c1 and c2 in india:
+    print("Both in india")
+elif c1 and c2 in pakistan:
+    print("Both in Pakistan")
+elif c1 and c2 in bangladesh:
+    print("Both in bangladesh")
+else:
+    print("Both are not in same country")
+    
+#3 
+sugar = int(input("Enter fasting sugar level:"))
+if sugar < 80:
+    print("Low sugar")
+elif sugar > 100:
+    print("High Sugar")
+else:
+    print("Normal Sugar")
