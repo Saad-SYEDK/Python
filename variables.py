f7e13be186d12ca5286c07c1b4b6722bd2f68b92
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2026-02-07 10:33:33 +0530

    Added more exercise in variables

--- variables.py
+++ variables.py
@@ -34,7 +34,7 @@ mod_result = a % b          # Modulus
 exp_result = a ** b         # Exponentiation
 
 
-"""Exercise
+"""Exercise - Variables
 1. Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
 
 2. Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
@@ -44,18 +44,7 @@ exp_result = a ** b         # Exponentiation
 4. Answer which of these are invalid variable names: _nation, 1record, record1, record_one record-one, record^one, continue .
 """
 
-
-
-
-
-
-
-
-
-
-
-
-# Solution to Exercise
+# Solution
 # 1. The variable name 'break' is a reserved keyword, and it cannot be used as a variable name. If you try to assign a value to 'break', you will get a SyntaxError.
 
 # 2.
@@ -72,3 +61,37 @@ full_name = first_name + " " + middle_name + " " + last_name
 print(full_name) # Output: Saad Syed Kaleemulla
 
 # 4. Invalid variable names: 1record, record-one, record^one, continue (because 'continue' is a reserved keyword in Python)
+
+"""Exercise - Numbers
+1. You have a football field that is 92 meter long and 48.8 meter wide. Find out total area using python and print it.
+2. You bought 9 packets of potato chips from a store. Each packet costs 1.49 dollar and you gave shopkeeper 20 dollar. Find out using python, how many dollars is the shopkeeper going to give you back?
+3. You want to replace tiles in your bathroom which is exactly square and 5.5 feet is its length. If tiles cost 500 rs per square feet, how much will be the total cost to replace all tiles. Calculate and print the cost using python (Hint: Use power operator ** to find area of a square)
+4. Print binary representation of number 17
+"""
+
+# Solution
+# 1.
+length = 92
+width = 48.8
+area = length * width
+print("Area of football field:", area, "square meters")
+
+# 2.
+packets = 9
+cost_per_packet = 1.49
+total_cost = packets * cost_per_packet
+amount_given = 20
+change = amount_given - total_cost
+print("Change to be given back:", change, "dollars")
+
+# 3.
+side_length = 5.5
+area_of_bathroom = side_length ** 2
+cost_per_square_feet = 500
+total_cost = area_of_bathroom * cost_per_square_feet
+print("Total cost to replace tiles:", total_cost, "rupees")
+
+# 4.
+number = 17
+binary_representation = bin(number)
+print("Binary representation of", number, "is", binary_representation)
