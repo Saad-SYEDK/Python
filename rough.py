53d0b2414b989fa5de679b285eaf3ad76a27412f
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2026-02-07 19:18:45 +0530

    rough

--- rough.py
+++ rough.py
@@ -1,10 +1,18 @@
-n = 4
+stocks = {}
+stocks["info"] = [600, 630, 620]
+stocks["ril"] = [1430, 1490, 1567]
+stocks["mtl"] = [234, 180, 160]
 
-def print_pattern(n):
-    for i in range(n):
-        for j in range(i+1):
-            print("*",end="")
-        print()
-
-
-print_pattern(n)
+while True:
+    ip = input("print, add (submit any other to exit): ")
+    if ip == "print":
+        for i, j in stocks.items():
+            print(i, "==>", j, "==>", "avg: ", round(sum(j)/len(j),2))
+    elif ip == "add":
+        ip = input("Enter Stock name: ")
+        if ip in stocks:
+            stocks[ip].append(int(input("Enter price: ")))
+        else:
+            stocks[ip] = [int(input("Enter Price: "))]
+    else:
+        break
