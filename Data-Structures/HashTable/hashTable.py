d25856c0ec32cbd7b04688e30b6a95894159f00e
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2026-02-09 16:27:12 +0530

    Fixed bugs in hash tables

--- Data-Structures/HashTable/hashTable.py
+++ Data-Structures/HashTable/hashTable.py
@@ -10,86 +10,53 @@
 
 class HashTable:
     def __init__(self):
-        self.Max = 100 # size of array
+        self.Max = 10
         self.arr = [None for _ in range(self.Max)]
+        self.count = 0
 
-    # THe magic of Hash Table is in its Hash Funciton- a takes any value and returns a hash value.
-    # THe Logic can be any thing, but it guarentees that it will return the same hash value for a same input
     def getHash(self, key):
         h = 0
         for char in key:
-            h += ord(char) # our logic is to add ascii value of each charactor and modulo
-        return h % self.Max # we do this to ensure that our values lies within the array index
-    
+            h += ord(char)
+        return h % self.Max
 
-    # The below fucntions do not handle collison, first understand the below code then moveon.
-    # We have to modify the same functions to handle collison 
-    """    
     def setItem(self, key, value):
-        h = self.getHash(key)     
-        self.arr[h] = (key, value)
+        if self.count >= self.Max:
+            raise Exception("Hash Table is full")
     
-    def getItem(self, key):
         h = self.getHash(key)
-        return self.arr[h] 
-    """
-
-    # We will implement linear probing to Handle Collision, look at different collision handling techniques 
-    
-    def setItem(self, key, value):
-        h = self.getHash(key) 
-        if self.arr[h] == None or self.arr[h][0] == key:
-            self.arr[h] = (key, value)
-        else: # Linear Probing - search for the next bucket below, if at last index go to first
-            full_counter = 0 # We will use this to flag if the array does not have any remaining space
-            while True:
-                if h ==self.Max-1:
-                
-                    if full_counter > 1: # If we have iterated through the loop twice
-                        raise Exception("The Underlying array in full !")
-                    full_counter+=1
-                
-                    h=0 # If we reached end of array we will start from begin ing
-                else:
-                    h+=1
-                    
-                if self.arr[h] == None or self.arr[h][0] == key:
-                    self.arr[h] = (key, value)
-                    break
-                 
-    def getItem(self, key):
-        h = self.getHash(key)
+        while self.arr[h] is not None and self.arr[h] != "DELETED":
+            if self.arr[h][0] == key: # Update existing key
+                break
+            h = (h + 1) % self.Max #Reset to 0 if h gets larger than max
         
-        if self.arr[h][0] == key:
-            return self.arr[h]
-        else:
-            full_counter = 0
-            while True:
-                if h == self.Max-1:
-                    if full_counter > 1:
-                        print("Key does not Exist!")
-                        return
-                    full_counter+=1                
-                    
-                    h = 0
-                else:
-                    h+=1
-                
-                if self.arr[h][0] == key:
-                    return self.arr[h]    
-            
-    
-    
-ht = HashTable()
+        if self.arr[h] is None or self.arr[h] == "DELETED":
+            self.count += 1
+        self.arr[h] = (key, value)
 
-for _ in range(100):
-    ht.setItem(str("saad"),69) 
+    def getItem(self, key):
+        h = self.getHash(key)
+        start_h = h
+        while self.arr[h] is not None:
+            if self.arr[h] != "DELETED" and self.arr[h][0] == key:
+                return self.arr[h][1]
+            h = (h + 1) % self.Max
+            if h == start_h: # Cycled back to start
+                break
+        return None
 
-# ht.setItem(str("saad"),69) 
-print(ht.arr)
-
-
-
+    def removeItem(self, key):
+        h = self.getHash(key)
+        start_h = h
+        while self.arr[h] is not None:
+            if self.arr[h] != "DELETED" and self.arr[h][0] == key:
+                self.arr[h] = "DELETED"
+                self.count -= 1
+                return True
+            h = (h + 1) % self.Max
+            if h == start_h:
+                break
+        return False
 """Exercise:
 Implement hash table where you handle collision with other techniques like seprate chaining or quadratic probing.
 """
