7ef1356e3371a7fd7c624ed1e3ee935015fb813f
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2026-02-11 16:42:28 +0530

    Added delete_node method in binaryTree

--- Data-Structures/Tree/binaryTree.py
+++ Data-Structures/Tree/binaryTree.py
@@ -2,8 +2,10 @@
 
 # Binary Search Tree(BST) is a BT that contains any order ex: at every node:
 #       left child will be smaller than parent and right child will be greater than the parent or vice-versa
-    
-class BT:
+
+
+# We will implement a simple binary tree and perform some operations on it like pre-order, in-order,etc.
+class BST:
     def __init__(self, data, left=None, right=None):
         self.data = data
         self.left = left
@@ -12,17 +14,17 @@ class BT:
     def add_node(self, data):
         if data == self.data:
             return
-        
+         
         if data > self.data:
             if self.right:
                 self.right.add_node(data)
             else:
-                self.right = BT(data)
+                self.right = BST(data)
         else:
             if self.left:
                 self.left.add_node(data)
             else:
-                self.left = BT(data)
+                self.left = BST(data)
     
     def pre_order(self):
         print(self.data)
@@ -36,7 +38,7 @@ class BT:
             self.left.in_order()
         print(self.data)
         if self.right:
-            self.right.in_order()
+            self.right.in_order()   
     
     def iterative_in_order(self):
         if self is None:
@@ -77,7 +79,9 @@ class BT:
             else:
                 return False
             
+    
     def find_min(self):
+    #    min value will be the leftmost leaf node in the tree
         itr = self
         while itr.left:
             itr = itr.left
@@ -85,6 +89,7 @@ class BT:
         return itr.data
     
     def find_max(self):
+    #    max value will be the rightmost leaf node in the tree 
         itr = self
         while itr.right:
             itr = itr.right
@@ -92,6 +97,10 @@ class BT:
         return itr.data
     
     def calculate_sum(self):
+        # We can do this using recursion as well but here we will do it iteratively using stack
+        # Add root node to the stack and keep popping the nodes until stack is empty. For every popped node, 
+            #  add its value to sum and add its left and right child to the stack if they exist.
+        # Time complexity of this algorithm is O(n) and space complexity is O(h) where h is the height of the tree.
         list = []
         list.append(self)
         sum = 0
@@ -107,23 +116,54 @@ class BT:
         return sum
 
 
+    def delete_node(self, val):
+        # While deleting a node, we can have 3 different cases:
+            # 1. Deleting a leaf node : we simply delete it and update the parent
+            # 2. Deleting a node with 1 child :
+                # if there is no leftchild, assign right child to current
+                # if there is no right child, assign left child to current
+            # 3. Deleting a node with 2 child, we have 2 options :
+            #   a. find the min value from the right node and replace with current node
+            #   b. find the max value from the left node and replace with current node
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete_node(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete_node(val)
+        else:
+            # Case 1
+            if not self.left and not self.right:
+                return None
+            
+            # Case 2
+            if not self.left:
+                return self.right
+            if not self.right:
+                return self.left
+            
+            # Case 3
+            #Find min_value from the right subtree
+            min_val = self.right.find_min() 
+            self.data = min_val
+            self.right = self.right.delete_node(min_val)
+        
+        return self
+            
+
 # Checking
 
-root = BT(5)
+root = BST(5)
 
 root.add_node(2)
 root.add_node(7)
 root.add_node(3)
 root.add_node(6)
-root.add_node(9)
-root.add_node(20
-              )
-root.add_node(10)
 
 root.in_order()
 
-print("Iteraive")
-root.iterative_in_order()
-
+# print("Iteraive")
+# root.iterative_in_order()
+root.delete_node(2)
 # print(root.calculate_sum())
     
