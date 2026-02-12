c0a51ae90f9b44c794f1a5f12e3403e18cf4aca6
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2026-02-12 14:15:40 +0530

    Created graph.py and added basic graph description

---
+++ Graph/graph.py
@@ -0,0 +1,12 @@
+# Similar to Trees, Graphs are also a non-linear data structure.
+# They consist of a finite set of vertices (or nodes) and a set of edges that connect pairs of vertices. 
+# Graphs can be directed or undirected, and they can also be weighted or unweighted.
+# Graphs are used to represent various real-world problems, such as social networks, transportation networks, and computer networks.
+
+# Graphs can be represented using an adjacency list(using linked list) or an adjacency matrix[2d array].
+# In an adjacency list, each vertex has a list of its adjacent vertices.
+    # It is a more space-efficient representation for sparse graphs, where the number of edges is much less than the square of the number of vertices.
+    
+    
+# In an adjacency matrix, a 2D array is used to represent the connections between vertices, where the value at row i and column j indicates whether there is an edge from vertex i to vertex j.
+    # It is a more space-efficient representation for dense graphs, where the number of edges is close to the square of the number of vertices.
