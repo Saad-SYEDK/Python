de63876b0ea58ed92c642901c344c29011acf3c3
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2026-02-14 10:20:07 +0530

    More explanation about OOP principles

--- oops.py
+++ oops.py
@@ -1,3 +1,23 @@
+# Object-Oriented Programming (OOP) is a programming paradigm that uses objects and classes to structure code.
+# OOP allows us to model real-world entities and their interactions in a more intuitive way.
+
+# The main principles of OOP are:
+
+# 1. Encapsulation: The bundling of data and methods that operate on that data within a single unit (class).
+    # promotes modularity and maintainability.
+
+# 2. Inheritance: The mechanism by which one class can inherit attributes and methods from another class,
+    # promoting code reusability and the creation of hierarchical relationships between classes.
+
+# 3. Polymorphism: The ability of different classes to be treated as instances of the same class through a common interface,
+        # allows flexibility and the ability to use objects of different types interchangeably.
+
+# 4. Abstraction: The concept of hiding the complex implementation details and showing only the necessary features of an object,
+    # allows for simpler interface and reducing complexity.
+
+
+
+
 # Class 
 
 # Class is a blueprint for creating objects. 
@@ -8,9 +28,6 @@
 
 # Object is an instance of a class. It is created using the class name followed by parentheses.
 # An object can have its own attributes and methods, which are defined in the class.
-
-
-
 
 
 # For more details and examples, look at this notes https://www.notion.so/Python-96ed6ff78c8240a79d1383120e9c1742?source=copy_link
@@ -50,7 +67,7 @@ class Employee(Human): # Employee is a child class of Human
         super().__init(name,  gender, occupation) # super() is used to call the __init__ method of the parent class
         self.salary = salary
     
-    @override # This is a decorator to indicate that we are overriding a method from the parent class
+    @override # This is a decorator to indicate that we are overriding a method from the parent class, we will learn more about overriding in the polymorphism.
     def work(self):
         print("I am working as a", self.occupation, "and I earn", self.salary)
 
