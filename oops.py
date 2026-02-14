0a9d85b334b63f2f8b8216447c575f16e2758847
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2026-02-14 10:12:03 +0530

    Added Inheritace explaination and codey

--- oops.py
+++ oops.py
@@ -15,7 +15,10 @@
 
 # For more details and examples, look at this notes https://www.notion.so/Python-96ed6ff78c8240a79d1383120e9c1742?source=copy_link
 
-class Human:
+from typing import override
+
+
+class Human: # Class - Blue Print
     def __init__(self, name, gender, occupation):
         self.name = name
         self.gender = gender
@@ -31,6 +34,31 @@ class Human:
         print("I am sleeping..zzzz")
 
 tc = Human("Tom Cruise", "Male", "Actor")
+#Here tc is OBJECT of CLASS Human, i.e tc follows the blueprint which we defined in Human
 tc.speak()
 tc.work()
 tc.sleep()
+
+
+# Inheritance:
+# Inheritance is a fundamental concept in object-oriented programming,
+    # It allows a class (called a child or subclass) to inherit attributes and methods from an another class (called a parent or superclass).
+# The child class can also have its own attributes and methods, and it can override the inherited methods.
+
+class Employee(Human): # Employee is a child class of Human
+    def __init__(self, name, gender, occupation, salary):
+        super().__init(name,  gender, occupation) # super() is used to call the __init__ method of the parent class
+        self.salary = salary
+    
+    @override # This is a decorator to indicate that we are overriding a method from the parent class
+    def work(self):
+        print("I am working as a", self.occupation, "and I earn", self.salary)
+
+emp1 = Employee("Alice", "Female", "Software Engineer", 50000)
+emp1.speak()
+emp1.work()
+emp1.sleep()
+
+# As we can see, the Employee class inherits the speak() and sleep() methods from the Human class,
+# Note: it overrides the work() method to provide a different implementation.
+# we use @override decorator to indicate that we are overriding a method from the parent class.
