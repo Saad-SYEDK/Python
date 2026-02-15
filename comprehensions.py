f2c4db0877506d7dc5dfa56f6d0c24c15913b433
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2026-02-15 09:32:55 +0530

    Notes with code complete

--- comprehensions.py
+++ comprehensions.py
@@ -5,7 +5,7 @@
 # Example
 
 
-numbers = [1, 2, 3, 4, 5, 6]
+numbers = [1, 2, 3, 4, 5, 6, 8, 10, 2, 6]
 
 # without list comprehesion
 even = []
@@ -27,3 +27,36 @@ squares = [i * i for i in range(10)]
 
 
 
+
+##############################################################################
+#  Set Comprehension
+######################################################################
+## Same as above for SETS COMPREHENSION - We only use {} brackets instead of []
+# Example:
+num = [1, 2, 3, 4,2, 4, 5,  5, 6, 6, 7, 6]
+# lets take even set of this list
+even_set = {i for i in num if i%2 == 0}
+print(even_set)
+
+
+
+
+############################################
+# Dictionary Comprehension
+###########################################
+
+city = ["Delhi", "Dubai", "New York"]
+Country = ["India", "UAE", "USA"]
+
+z = zip(Country, city) # This creates a zip object, which we can itereate through
+print(z)
+for i in z:
+    print(i)
+    
+# Dict comprehension
+dictionary = {co:ci for co,ci in zip(Country, city)}
+print(dictionary)
+
+
+#####################
+#Comprehension is a very powerful feature, the litmit is the sky, we can use complex logic to create required list without having to write tones of code
