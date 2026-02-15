86bf5c008b44913d290cd851e918e8e0a85a7880
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2026-02-15 10:00:28 +0530

    Code and explaination

--- sets.py
+++ sets.py
@@ -25,3 +25,25 @@ ss = {1} # This is how we create a set with one element
 ss = set() # This is how we create an empty set
 
 
+# If we want to create a immotable set, we can use the frozenset() constructor.
+my_frozenset = frozenset([1, 2, 3, 4, 5])
+# my_frozenset.add(6) # This will raise an error because frozensets are immutable
+
+
+# Sets operations:
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+# union - returns a set that contains all the elements from both sets, without duplicates.
+set3 = set1.union(set2) # or set3 = set1 | set2 (or)
+
+# intersection - returns a set that contains all the elements that are common to both sets.
+set4 = set1.intersection(set2) # or set4 = set1 & set2 (and)
+
+# difference
+set5 = set1.difference(set2) # or set5 = set1 - set2 (minus)    
+
+#  symmetric difference - returns a set that contains all the elements that are in either set1 or set2, but not in both.
+set6 = set1.symmetric_difference(set2) # or set6 = set1 ^ set2 (xor)
+
+
+# Look at the set methods for more operations like issubset(), issuperset(), isdisjoint(), etc.
